f0d343d49be066f5a7194a4a20ca04f82545d807
Yannick Fricke <yannickfricke@googlemail.com>
2023-01-17 07:23:51 +0100

    feat: added the UseInvoker module
    The UseInvoker module can be used for generating new modules
    which itself gets a using directive that accepts an atom as first
    argument.
    
    If the `required_utils` option is passed then the calling module also
    invokes the passed function names (that must be arguments) to ensure
    that all the specified core utilities will be available.

---
+++ lib/hype_lib/use_invoker.ex
@@ -0,0 +1,110 @@
+defmodule HypeLib.UseInvoker do
+  @moduledoc """
+  The `HypeLib.UseInvoker` module offers a use macro for utility modules.
+
+  The macro defines a `__using__` macro in the **CALLING MODULE** where the first argument is
+  expected to be an atom so we can pass it directly to the `apply/3` function as second argument.
+
+  The macro supports the following options:
+
+  > **Name:**
+  >
+  > required_utils
+  >
+  > **Expected data type:**
+  >
+  > list(atom)
+  >
+  > **Description:**
+  >
+  > A list of function names (atoms) which should always be invoked when
+  > the macro is called.
+
+
+  ## Examples
+
+  ### Without any arguments
+
+  ```elixir
+  iex> defmodule MyUtils do
+  ...>   use HypeLib.UseInvoker
+  ...>
+  ...>   def num() do
+  ...>     quote do
+  ...>       def add(a, b), do: a + b
+  ...>     end
+  ...>   end
+  ...> end
+  ...>
+  ...> defmodule MyConsumer do
+  ...>   use MyUtils, :num
+  ...>
+  ...>   def my_add(a, b), do: add(a, b)
+  ...> end
+  ...>
+  ...> MyConsumer.my_add(1, 2)
+  3
+  ```
+
+  ### With required_utils
+
+  ```elixir
+  iex> defmodule MyUtils do
+  ...>   use HypeLib.UseInvoker, required_utils: [:core]
+  ...>
+  ...>   def core do
+  ...>     quote do
+  ...>       def core_fun(), do: "core function"
+  ...>     end
+  ...>   end
+  ...>
+  ...>   def num() do
+  ...>     quote do
+  ...>       def add(a, b), do: a + b
+  ...>     end
+  ...>   end
+  ...> end
+  ...>
+  ...> defmodule MyConsumer do
+  ...>   use MyUtils, :num
+  ...> end
+  ...>
+  ...> MyConsumer.core_fun()
+  "core function"
+  ```
+
+  """
+
+  defmacro __using__(args) do
+    required_utils = Keyword.get(args, :required_utils, [])
+
+    quote do
+      use TypeCheck
+
+      @spec __using__(which :: atom() | list(atom())) :: Macro.t()
+      defmacro __using__(which) when is_atom(which) do
+        apply_using([which])
+      end
+
+      defmacro __using__(which) when is_list(which) do
+        apply_using(which)
+      end
+
+      defp apply_using(which) do
+        which = Enum.uniq(unquote(required_utils) ++ which)
+
+        Enum.reduce(
+          which,
+          quote do
+          end,
+          fn entry, acc ->
+            quote do
+              unquote(acc)
+              unquote(apply(__MODULE__, entry, []))
+            end
+          end
+        )
+      end
+    end
+  end
+end
