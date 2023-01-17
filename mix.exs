456b8d39bb0547a5cf2e3beb921f0775dda31766
Yannick Fricke <yannickfricke@googlemail.com>
2023-01-17 08:53:11 +0100

    build(mix): added missing coveralls envs settings

--- mix.exs
+++ mix.exs
@@ -33,7 +33,13 @@ defmodule HypeLib.MixProject do
       ],
 
       # Coveralls
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
