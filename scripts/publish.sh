0111ca0095504763d04be40c899fc36c52e6a91f
Yannick Fricke <yannickfricke@googlemail.com>
2023-01-17 13:33:13 +0100

    build: refactored the publish.sh script
    The script now uses colors and stashes the current changes before
    performing the release.
    We also switch back to the branch which was active before the script was
    started and pops the latest stash which was created before the release
    was going to be performed.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,11 +3,12 @@
 current_version=$(cat mix.exs | grep -P "version: \"(.*)\"")
 new_version="0.0.0"
 regex="\"(.*)\""
+old_branch=$(git branch --show-current)
 
 function exit_with_message () {
-    echo $1
+    echo -e "\e[91m$1\e[39m"
     echo
-    echo "Exiting with code $2"
+    echo -e "\e[91mExiting with code $2\e[39m"
 
     exit $2
 }
@@ -19,8 +20,16 @@ function output_version () {
 function step () {
     echo "-----"
     echo ""
-    echo "Step: $1"
+    echo -e "Step: \e[94m$1\e[39m"
     echo ""
+}
+
+function sub_step () {
+    echo -e "-> $1"
+}
+
+function sub_sub_step () {
+    echo -e "  -> $1"
 }
 
 step "Checking command line arguments"
@@ -29,9 +38,14 @@ if [ "$#" -ne 1 ]; then
     exit_with_message "Usage: $0 <new version string>" 1
 fi
 
+step "Stashing current changes"
+git stash save "Latest stash on branch $old_branch"
+
+step "Resetting git the hard way"
+git reset --hard
+
 step "Switching to master branch"
 git checkout master
-
 
 step "Fetching current version from mix.exs"
 
@@ -55,11 +69,10 @@ sed -i -e "/~>/s/\"~> .*\"/\"~> $new_version\"/" README.md
 step "Replace upcoming version in all lib/**/*.ex files"
 
 for file in $(find ./lib -type f -name "*.ex"); do
-    echo "Processing file: $file"
+    sub_step "Processing file: \e[36m$file\e[39m"
 
     sed -i -e "/###/s/### <upcoming version>/### $new_version/" $file
 done
-
 
 step "Adding mix.exs and README.md to git"
 git add mix.exs README.md ./lib/
@@ -75,3 +88,42 @@ git push
 
 step "Push the new tag to GitHub"
 git push --tags
+
+all_branches=()
+branches_to_rebase=()
+eval "$(git for-each-ref --shell --format='all_branches+=(%(refname:short))' refs/heads/)"
+
+step "Find local branches to rebase"
+
+for branch in "${all_branches[@]}"; do
+    if [[ $branch != "master" ]]; then
+        branches_to_rebase+=($branch)
+
+        sub_step "Found local branch to rebase: $branch"
+    fi
+done
+
+if [ "${#branches_to_rebase[@]}" -ne 0 ]; then
+    step "Rebasing local branches"
+
+    for branch in "${branches_to_rebase[@]}"; do
+        sub_step "Rebasing branch $branch"
+        
+        git checkout $branch
+        git rebase master
+    done
+else
+    sub_step "\e[92m! No local branches to rebase found !\e[39m"
+    echo
+fi
+
+step "Checking out old branch \e[0m- \e[95m$old_branch\e[39m"
+git checkout $old_branch
+
+step "Popping latest stash"
+git stash pop
+
+echo
+echo "-----"
+echo -e "Published new version: \e[5;92m$new_version\e[25;39m!"
+echo "Also switched back to old branch $old_branch and applied your lastest changes"
