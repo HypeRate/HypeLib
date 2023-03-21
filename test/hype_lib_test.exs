af762aaa36da2ac625e6b2f88f5887bde4a9505b
Yannick Fricke <yannickfricke@googlemail.com>
2023-03-21 02:20:10 +0100

    test(url): added the Utils.Url module to the tests

--- test/hype_lib_test.exs
+++ test/hype_lib_test.exs
@@ -1,8 +1,17 @@
 defmodule HypeLibTest do
+  @moduledoc """
+  ## Changelog
+
+  ### <upcoming version>
+
+  Added the `HypeLib.Utils.Url` module to the modules to run DocTests for
+  """
+
   use HypeLib.DocTester,
     modules: [
       HypeLib.Prelude,
       HypeLib.UseInvoker,
-      HypeLib.Utils.String.Generator
+      HypeLib.Utils.String.Generator,
+      HypeLib.Utils.Url
     ]
 end
