064dcaafbe802a92c62e512bb4a7d3c616e14861
Yannick Fricke <yannickfricke@googlemail.com>
2023-03-21 02:49:45 +0100

    refactor(changeset): added charset support for `put_generated_string_if_needed/4`

--- lib/hype_lib/utils/changeset.ex
+++ lib/hype_lib/utils/changeset.ex
@@ -16,23 +16,33 @@ defmodule HypeLib.Utils.Changeset do
   alias HypeLib.Utils
 
   @doc """
+  If the underlying struct contains a non-nil value for the given field, then
+  the function will generate a new random string and puts it into the changeset.
+
+  The `charset` argument should be generated through the `HypeLib.Utils.String.Generator.charsets/1`
+  or `HypeLib.Utils.String.Generator.charsets!/1` functions.
+
   ## Changelog
 
   ### 2.3.0
 
   First implementation
+
+  ### <upcoming version>
+
+  Added the `charset` argument. This gives developers more control over the used characters
+  which should be used when generating the random string.
   """
   def put_generated_string_if_needed(
         %Ecto.Changeset{} = changeset,
         field,
+        charset,
         generated_string_length \\ 32
       ) do
     case get_field(changeset, field) do
       nil ->
-        charsets = Utils.String.Generator.charsets!([:lower_hex])
-
         generated_string =
-          Utils.String.Generator.generate_string!(generated_string_length, charsets)
+          Utils.String.Generator.generate_string!(generated_string_length, charset)
 
         put_change(changeset, field, generated_string)
 
