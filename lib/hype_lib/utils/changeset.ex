1aaff00d6dd036acb9c2e40ce2c0c7057bda7c43
Yannick Fricke <yannickfricke@googlemail.com>
2023-03-21 01:54:17 +0100

    feat(utils): added the Changeset util module
    For now it only exports the `put_generated_string_if_needed/3` function
    which puts a randomly generated string into a changeset when the
    underlying struct does not have valid value for the given field (second
    argument).

---
+++ lib/hype_lib/utils/changeset.ex
@@ -0,0 +1,31 @@
+defmodule HypeLib.Utils.Changeset do
+  alias HypeLib.Utils
+
+  import Ecto.Changeset
+
+  @doc """
+  ## Changelog
+
+  ### <upcoming version>
+
+  First implementation
+  """
+  def put_generated_string_if_needed(
+        %Ecto.Changeset{} = changeset,
+        field,
+        generated_string_length \\ 32
+      ) do
+    case get_field(changeset, field) do
+      nil ->
+        charsets = Utils.String.Generator.charsets!([:lower_hex])
+
+        generated_string =
+          Utils.String.Generator.generate_string!(generated_string_length, charsets)
+
+        put_change(changeset, field, generated_string)
+
+      _ ->
+        changeset
+    end
+  end
+end
